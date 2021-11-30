b7ff9f28c7e5b98c37773db32d47a064d5b430b3
Nate Mills <mills.nate@gmail.com>
2021-11-29 18:08:26 -0800

    solved aoc 2018 day 3

--- adventofcode/2018/run.py
+++ adventofcode/2018/run.py
@@ -1,14 +1,13 @@
-import day2 as Day
+import day3 as Day
 
 
 inFile = open("testInputs/test1.in", "r").read().split("\n")
 inFile.pop()
 # print(str(Day.part2(inFile)))
 
-
 # exit()
 
-day = "2"
+day = "3"
 inFile = open("inputs/day" + day + ".in", "r").read().split("\n")
 inFile.pop()
 print("day" + day + " part1: " + str(Day.part1(inFile)))
