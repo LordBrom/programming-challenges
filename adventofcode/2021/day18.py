20132f28ccaa7c811cf52ab8fb9b3aacbf85ba34
Nate Mills <mills.nate@gmail.com>
2021-12-18 13:59:53 -0800

    adding test files

--- adventofcode/2021/day18.py
+++ adventofcode/2021/day18.py
@@ -5,15 +5,15 @@ import random
 class SnailNum():
     def __init__(self, data, depth=0, parent=None):
         self.randID = random.randint(1, 10000)
-        self.first = None
-        self.firstIsNum = None
-        self.last = None
-        self.lastIsNum = None
+        self.left = None
+        self.leftIsNum = None
+        self.right = None
+        self.rightIsNum = None
         self.depth = depth
         self.parentSnailNum = parent
 
         data = data[1:-1]
-        lastStart = data.find(",", 1)
+        rightStart = data.find(",", 1)
         if data[0] == "[":
             openPos = 1
             closePos = 1
@@ -24,18 +24,16 @@ class SnailNum():
                 closePos = nextClose + 1
                 nextOpen = data.find("[", openPos)
                 nextClose = data.find("]", closePos)
-            lastStart = data.find(",", nextClose)
-            self.first = SnailNum(data[0:nextClose + 1], depth + 1, self)
-            self.firstIsNum = False
+            rightStart = data.find(",", nextClose)
+            self.left = SnailNum(data[0:nextClose + 1], depth + 1, self)
+            self.leftIsNum = False
         else:
-            # print(data[0:lastStart])
-            self.first = int(data[0:lastStart])
-            self.firstIsNum = True
+            self.left = int(data[0:rightStart])
+            self.leftIsNum = True
 
-        lastEnd = data.find("]", lastStart)
-        if data[lastStart + 1] == "[":
-            openPos = lastStart + 2
-            closePos = lastStart + 2
+        if data[rightStart + 1] == "[":
+            openPos = rightStart + 2
+            closePos = rightStart + 2
             nextOpen = data.find("[", openPos)
             nextClose = data.find("]", closePos)
             while not (nextOpen == -1 or nextOpen > nextClose):
@@ -43,147 +41,139 @@ class SnailNum():
                 closePos = nextClose + 1
                 nextOpen = data.find("[", openPos)
                 nextClose = data.find("]", closePos)
-            self.last = SnailNum(
-                data[lastStart + 1:nextClose + 1], depth + 1, self)
-            self.lastIsNum = False
+            self.right = SnailNum(
+                data[rightStart + 1:nextClose + 1], depth + 1, self)
+            self.rightIsNum = False
         else:
-            # print(data[lastStart + 1:])
-            self.last = int(data[lastStart + 1:])
-            self.lastIsNum = True
+            self.right = int(data[rightStart + 1:])
+            self.rightIsNum = True
 
         pass
 
     def addition(self, snailNum):
         return SnailNum('[' + self.printNum(False) + ',' + snailNum.printNum(False) + ']')
 
-    def addFirst(self, amount, sameSide=True, goingUp=False):
-        if self.firstIsNum:
-            self.first += amount
+    def addLeft(self, amount, sameSide=True, goingUp=False):
+        if self.leftIsNum:
+            self.left += amount
         else:
             if goingUp:
-                self.first.addFirst(amount, sameSide, goingUp)
+                self.left.addLeft(amount, sameSide, goingUp)
             elif self.depth != 0 and sameSide:
                 newSameSide = (
-                    not self.parentSnailNum.firstIsNum) and self.parentSnailNum.first.randID == self.randID
+                    not self.parentSnailNum.leftIsNum) and self.parentSnailNum.left.randID == self.randID
 
-                self.parentSnailNum.addFirst(
+                self.parentSnailNum.addLeft(
                     amount, newSameSide)
-            elif self.depth == 0:
+            elif self.depth == 0 and sameSide:
                 pass
             else:
-                self.first.addLast(
+                self.left.addRight(
                     amount, True, True)
 
-    def addLast(self, amount, sameSide=True, goingUp=False):
-        if self.lastIsNum:
-            self.last += amount
+    def addRight(self, amount, sameSide=True, goingUp=False):
+        if self.rightIsNum:
+            self.right += amount
         else:
             if goingUp:
-                self.last.addLast(amount, sameSide, goingUp)
+                self.right.addRight(amount, sameSide, goingUp)
             elif self.depth != 0 and sameSide:
                 newSameSide = (
-                    not self.parentSnailNum.lastIsNum) and self.parentSnailNum.last.randID == self.randID
-                self.parentSnailNum.addLast(
+                    not self.parentSnailNum.rightIsNum) and self.parentSnailNum.right.randID == self.randID
+                self.parentSnailNum.addRight(
                     amount, newSameSide)
-            elif self.depth == 0:
+            elif self.depth == 0 and sameSide:
                 pass
             else:
-                self.last.addFirst(
+                self.right.addLeft(
                     amount, True, True)
 
     def reduce(self):
-        current = self.printNum(False)
-        last = ""
         step = 0
         while True:
-            self.printNum()
             step += 1
-            last = current
-            self.expload()
-            current = self.printNum(False)
 
-            if last != current:
+            if self.expload()[0]:
                 continue
 
-            last = current
-            self.split()
-            # print("before", last)
-            current = self.printNum(False)
+            if self.split():
+                continue
 
-            if last == current:
-                break
+            break
 
-    def expload(self):
-        if not self.firstIsNum:
-            firstExploaded = self.first.expload()
-            if firstExploaded != None:
-                self.addLast(firstExploaded[1], False)
-                self.addFirst(firstExploaded[0])
-                self.firstIsNum = True
-                self.first = 0
+    def expload(self, hasExploaded=False):
+        if not self.leftIsNum:
+            hasExploaded, leftExploaded = self.left.expload(hasExploaded)
+            if leftExploaded != None:
+                self.addLeft(leftExploaded[0])
+                self.addRight(leftExploaded[1], False)
+                self.leftIsNum = True
+                self.left = 0
 
-        if not self.lastIsNum:
-            lastExploaded = self.last.expload()
-            if lastExploaded != None:
-                self.addLast(lastExploaded[1])
-                self.addFirst(lastExploaded[0], False)
-                self.lastIsNum = True
-                self.last = 0
+        if not self.rightIsNum:
+            hasExploaded, rightExploaded = self.right.expload(hasExploaded)
+            if rightExploaded != None:
+                self.addLeft(rightExploaded[0], False)
+                self.addRight(rightExploaded[1])
+                self.rightIsNum = True
+                self.right = 0
 
-        if self.depth >= 4:
-            return (self.first, self.last)
+        if not hasExploaded and self.depth >= 4:
+            return (True, (self.left, self.right))
 
-    def split(self):
-        didSplit = False
-        if not didSplit and self.firstIsNum and self.first >= 10:
-            self.firstIsNum = False
-            self.first = SnailNum(
-                "[" + str(math.floor(self.first / 2)) + "," + str(math.ceil(self.first / 2)) + "]")
-            self.first.depth = self.depth + 1
-            self.first.parentSnailNum = self
+        return (hasExploaded, None)
+
+    def split(self, didSplit=False):
+        if not didSplit and self.leftIsNum and self.left >= 10:
+            self.leftIsNum = False
+            self.left = SnailNum(
+                "[" + str(math.floor(self.left / 2)) + "," + str(math.ceil(self.left / 2)) + "]")
+            self.left.depth = self.depth + 1
+            self.left.parentSnailNum = self
             didSplit = True
-        elif not didSplit and not self.firstIsNum:
-            didSplit = self.first.split()
+        elif not didSplit and not self.leftIsNum:
+            didSplit = self.left.split(didSplit)
 
-        if not didSplit and self.lastIsNum and self.last >= 10:
-            self.lastIsNum = False
-            self.last = SnailNum(
-                "[" + str(math.floor(self.last / 2)) + "," + str(math.ceil(self.last / 2)) + "]")
-            self.last.depth = self.depth + 1
-            self.last.parentSnailNum = self
+        if not didSplit and self.rightIsNum and self.right >= 10:
+            self.rightIsNum = False
+            self.right = SnailNum(
+                "[" + str(math.floor(self.right / 2)) + "," + str(math.ceil(self.right / 2)) + "]")
+            self.right.depth = self.depth + 1
+            self.right.parentSnailNum = self
             didSplit = True
-        elif not didSplit and not self.lastIsNum:
-            didSplit = self.last.split()
+        elif not didSplit and not self.rightIsNum:
+            didSplit = self.right.split(didSplit)
+
         return didSplit
 
     def getMagnitude(self):
-        lastNum = 0
-        firstNum = 0
-        if self.firstIsNum:
-            firstNum = self.first
+        rightNum = 0
+        leftNum = 0
+        if self.leftIsNum:
+            leftNum = self.left
         else:
-            firstNum = self.first.getMagnitude()
+            leftNum = self.left.getMagnitude()
 
-        if self.lastIsNum:
-            lastNum = self.last
+        if self.rightIsNum:
+            rightNum = self.right
         else:
-            lastNum = self.last.getMagnitude()
+            rightNum = self.right.getMagnitude()
 
-        return (3 * firstNum) + (2 * lastNum)
+        return (3 * leftNum) + (2 * rightNum)
 
     def printNum(self, printStr=True):
         outStr = "["
-        if self.firstIsNum:
-            outStr += str(self.first)
+        if self.leftIsNum:
+            outStr += str(self.left)
         else:
-            outStr += self.first.printNum(False)
+            outStr += self.left.printNum(False)
 
         outStr += ","
 
-        if self.lastIsNum:
-            outStr += str(self.last)
+        if self.rightIsNum:
+            outStr += str(self.right)
         else:
-            outStr += self.last.printNum(False)
+            outStr += self.right.printNum(False)
         outStr += "]"
 
         if printStr:
@@ -195,15 +185,25 @@ def part1(data):
     result = SnailNum(data[0])
 
     for i in range(1, len(data)):
-        startNum = result.printNum(False)
-        newNum = SnailNum(data[i])
         result = result.addition(SnailNum(data[i]))
         result.reduce()
-        print(startNum, " + ", newNum.printNum(False),
-              " = ", result.printNum(False))
+    result.reduce()
 
     return result.getMagnitude()
 
 
 def part2(data):
-    return "not implemented"
+    result = 0
+    for x in range(len(data)):
+        for y in range(len(data)):
+            newCheck = SnailNum(data[x])
+            newCheck = newCheck.addition(SnailNum(data[y]))
+            newCheck.reduce()
+            result = max(result, newCheck.getMagnitude())
+
+            swapCheck = SnailNum(data[y])
+            swapCheck = swapCheck.addition(SnailNum(data[x]))
+            swapCheck.reduce()
+            result = max(result, swapCheck.getMagnitude())
+
+    return result
