9bab9c53bc724981d5f8deacee49debb5a81e5f8
Nate Mills <mills.nate@gmail.com>
2021-12-30 10:56:14 -0800

    AoC 2018 day7

--- adventofcode/2018/day7.py
+++ adventofcode/2018/day7.py
@@ -1,9 +1,142 @@
+import re
 
-RESTR = "Step ([A-Z]) must be finished before step ([A-Z]) can begin."
-
-def part1(input):
-    return "not implemented"
+RE_STR = "Step ([A-Z]) must be finished before step ([A-Z]) can begin."
 
 
-def part2(input):
-    return "not implemented"
+class Point():
+    def __init__(self, name) -> None:
+        self.name = name
+        self.before = []
+        self.after = []
+
+    def __str__(self) -> str:
+        result = "Step " + self.name + " must be finished before step(s) "
+        first = True
+        for p in self.after:
+            if not first:
+                result += ", "
+            first = False
+            result += p.name
+
+        result += " can begin, and after "
+        first = True
+        for p in self.before:
+            if not first:
+                result += ", "
+            first = False
+            result += p.name
+
+        result += " have begin."
+        return result
+
+    def addBeforePoint(self, beforePoint):
+        self.before.append(beforePoint)
+
+    def addAfterPoint(self, afterPoint):
+        self.after.append(afterPoint)
+
+    def canBeAdded(self, result):
+        for p in self.before:
+            if not p.name in result:
+                return False
+        return True
+
+
+class Worker():
+    def __init__(self, extraTime=60) -> None:
+        self.workingOn = "."
+        self.progress = 0
+        self.extraTime = extraTime
+
+    def __str__(self) -> str:
+        return self.workingOn
+
+    def startWorking(self, letter):
+        if self.workingOn != ".":
+            return False
+        self.workingOn = letter
+        self.progress = (ord(letter) - 64) + self.extraTime
+        return True
+
+    def tickSecond(self):
+        if self.workingOn == ".":
+            return None
+        self.progress -= 1
+        if self.progress == 0:
+            result = self.workingOn.upper()
+            self.workingOn = "."
+            return result
+
+
+def parsePointList(data):
+    pointList = {}
+    for d in data:
+        reResult = re.search(RE_STR, d)
+        thisPoint = reResult.group(1)
+        afterPoint = reResult.group(2)
+
+        if not thisPoint in pointList:
+            pointList[thisPoint] = Point(thisPoint)
+        if not afterPoint in pointList:
+            pointList[afterPoint] = Point(afterPoint)
+        pointList[afterPoint].addBeforePoint(pointList[thisPoint])
+        pointList[thisPoint].addAfterPoint(pointList[afterPoint])
+    return pointList
+
+
+def part1(data):
+    pointList = parsePointList(data)
+
+    result = ""
+    while len(result) < len(pointList):
+        available = []
+        for p in pointList:
+            if not p in result and pointList[p].canBeAdded(result):
+                available.append(p)
+        available.sort()
+        result += available[0]
+
+    return result
+
+
+def part2(data):
+    pointList = parsePointList(data)
+    workerCount = 5
+    workers = []
+    for i in range(workerCount):
+        workers.append(Worker())
+
+    second = 0
+    result = ""
+    beingWorked = ""
+    while len(result) < len(pointList):
+        for w in workers:
+            addLetter = w.tickSecond()
+            if addLetter != None:
+                result += addLetter
+
+        available = []
+        for p in pointList:
+            if not p in beingWorked and pointList[p].canBeAdded(result):
+                available.append(p)
+        available.sort()
+        nextIndex = 0
+
+        for w in workers:
+            if len(available) <= nextIndex:
+                break
+            newWork = w.startWorking(available[nextIndex])
+            if newWork:
+                beingWorked += available[nextIndex]
+                nextIndex += 1
+
+        outStr = "sec: " + str(second)
+        for i in range(len(workers)):
+            outStr += " w" + str(i) + ": " + str(workers[i])
+        outStr += " result: " + result
+
+        # print(outStr)
+        # input()
+        second += 1
+
+    return second - 1
--- adventofcode/2018/run.py
+++ adventofcode/2018/run.py
@@ -1,12 +1,54 @@
-import day6 as Day
+import time
+import day7 as Day
 
+day = "7"
 
-day = "6"
+runTest = input("(1) Run test; (2/else) Run actual: ")
+runPart1 = input("Run part 1 (y)/else: ")
 
-#inFile = open("testInputs/test1.in", "r").read().split("\n")
-inFile = open("inputs/day" + day + ".in", "r").read().split("\n")
+if runTest == "1":
+    inFile = open("inputs/day" + day + ".in", "r").read().split("\n")
+else:
+    inFile = open("testInputs/day" + day + ".in", "r").read().split("\n")
 inFile.pop()
 
+if len(inFile) == 1:
+    inPart1 = inFile.copy()[0]
+    inPart2 = inFile.copy()[0]
+else:
+    inPart1 = inFile.copy()
+    inPart2 = inFile.copy()
 
-print("day" + day + " part1: " + str(Day.part1(inFile)))
-print("day" + day + " part2: " + str(Day.part2(inFile)))
+
+def outputAnswer(part, answer, time):
+    timeStr = str("{:,.0f}".format(time)) + " ms"
+    print(
+        "| Part {} | {} | {} |".format(
+            part, answer.ljust(50, " "), timeStr.rjust(15, " ")
+        )
+    )
+
+
+def timeMs():
+    return int(round(time.time() * 1000))
+
+
+def printLine():
+    print("-" * 81)
+
+
+printLine()
+dayDsp = day
+if len(day) == 1:
+    dayDsp += " "
+print("| Day {} | Answer{} | Run Time{} |".format(dayDsp, " " * 44, " " * 7))
+printLine()
+
+if runPart1 == "":
+    startTime = timeMs()
+    outputAnswer("1", str(Day.part1(inPart1)), timeMs() - startTime)
+    printLine()
+
+startTime = timeMs()
+outputAnswer("2", str(Day.part2(inPart2)), timeMs() - startTime)
+printLine()
