56eb29da4f151ee8728c0f79704f15eff7164335
Nate Mills <mills.nate@gmail.com>
2022-02-23 16:04:43 -0800

    solivng 2015 day 20

--- adventofcode/2015/day20.py
+++ adventofcode/2015/day20.py
@@ -1,7 +1,41 @@
 
+from functools import reduce
+
+
+def getFactors(n):
+    return list(set(reduce(list.__add__,
+                           ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
+
+
+def countPresents(houseNum, presents=10, maxHouses=False):
+    result = 0
+    factors = getFactors(houseNum)
+    factors.sort(reverse=True)
+    for n in factors:
+        if maxHouses and houseNum / n > 50:
+            break
+        result += n * presents
+    return result
+
+
 def part1(data):
-    return "not implemented"
+    house = 1
+    house = 786240
+
+    while True:
+        check = countPresents(house)
+        if check >= int(data):
+            break
+        house += 1
+    return house
 
 
 def part2(data):
-    return "not implemented"
+    house = 1
+
+    while True:
+        check = countPresents(house, 11, True)
+        if check >= int(data):
+            break
+        house += 1
+    return house
--- adventofcode/2015/run.py
+++ adventofcode/2015/run.py
@@ -1,10 +1,10 @@
 import sys
 import time
-import day19 as Day
+import day20 as Day
 
 
 def main(args=None):
-    day = "19"
+    day = "20"
 
     runTest = input("(1) Run test; (2/else) Run actual: ")
     runPart1 = input("Run part 1 (y)/else: ")
