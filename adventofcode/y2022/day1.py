956e52edc87bbbef862a97faea5e316bc45bace3
Nate Mills <mills.nate@gmail.com>
2022-11-30 21:10:53 -0800

    solving aoc 2022 day 1

--- adventofcode/y2022/day1.py
+++ adventofcode/y2022/day1.py
@@ -1,7 +1,27 @@
- 
-def part1(data, test=False) -> str: 
-    return "not implemented" 
+
+def part1(data, test=False) -> str:
+	elves = [0]
+	for n in data:
+		if n == "":
+			elves.append(0)
+			continue
+		elves[-1] += int(n)
+
+	elves.sort()
+	return str(elves[-1])
 
 
-def part2(data, test=False) -> str: 
-    return "not implemented" 
+def part2(data, test=False) -> str:
+	elves = [0]
+	for n in data:
+		if n == "":
+			elves.append(0)
+			continue
+		elves[-1] += int(n)
+
+	maxElf = 0
+	for e in elves:
+		maxElf = max(e, maxElf)
+
+	elves.sort()
+	return str(elves[-1] + elves[-2] + elves[-3])
