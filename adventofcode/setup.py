96c89e9b0b913a944715b644a5e28f734cda0acc
Nate Mills <mills.nate@gmail.com>
2023-12-09 18:16:12 -0800

    adding str() to function template

--- adventofcode/setup.py
+++ adventofcode/setup.py
@@ -7,11 +7,11 @@ def main(args=None):
 
     default = ' \n\
 def part1(data, test=False) -> str: \n\
-    return "not implemented" \n\
+    return str("not implemented") \n\
 \n\
 \n\
 def part2(data, test=False) -> str: \n\
-    return "not implemented" \n\
+    return str("not implemented") \n\
 '
     mainPath = "y" + year + "/day" + day + ".py"
     inputPath = "y" + year + "/inputs/day" + day + ".in"
